79796bf75092a4e95d0358eba8cbd964bd5cea90
Becki Robards <becki.robards@gmail.com>
2019-04-03 19:48:17 -0400

    Created initial project file

---
+++ MastermindConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MastermindConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
